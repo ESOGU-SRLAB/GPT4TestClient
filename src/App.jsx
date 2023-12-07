805d54ca2e0f5d7d0ef7e3fad1e471ea94e88809
Metin KONUK <metinkonuk@hotmail.com>
2023-12-07 17:42:45 +0300

    fix dependency issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Header from './components/Header'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,12 +8,11 @@ function App() {
 
   return (
     <>
-      <Header/>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
