005b5dde821172ec703ef06ee56a2a3aa468171b
Metin KONUK <metinkonuk@hotmail.com>
2023-12-18 00:18:37 +0300

    handle async authentication operations using redux-async-thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counterSlice";
-import userDataReducer from "./features/userDataSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { loadUserDataFromLocalStorage } from '../utils/localStorage';
+import counterReducer from './features/counterSlice';
+import userDataReducer from './features/userDataSlice';
+
+const preloadedState = {
+    userData: loadUserDataFromLocalStorage(),
+};
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        userData: userDataReducer
-    }
+        userData: userDataReducer,
+    },
+    preloadedState,
 });
--- src/redux/features/userDataSlice.js
+++ src/redux/features/userDataSlice.js
@@ -1,19 +1,106 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { saveUserDataToLocalStorage } from '../../utils/localStorage';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+export const handleLogout = (navigate) => (dispatch) => {
+    localStorage.removeItem('userData');
+    dispatch(logoutUser());
+    navigate('/login');
+};
+
+// Async thunk for logging in a user
+export const loginUser = createAsyncThunk(
+    'userData/loginUser',
+    async ({ identifier, password }, { dispatch }) => {
+        try {
+            const response = await axios.post(
+                'http://localhost:5000/api/users/login',
+                {
+                    identifier,
+                    password,
+                }
+            );
+
+            // Dispatch action to set user data on successful login
+            dispatch(
+                setUserData({
+                    username: response.data.username,
+                    userEmailAddress: response.data.email,
+                })
+            );
+
+            saveUserDataToLocalStorage({
+                username: response.data.username,
+                userEmailAddress: response.data.email,
+            });
+            toast.success('Logged in successfully');
+            return response.data;
+        } catch (error) {
+            const errorMessage =
+                error.response?.data?.message || 'Unknown login error';
+            toast.error(errorMessage);
+        }
+    }
+);
+
+// Async thunk for registering a user
+export const registerUser = createAsyncThunk(
+    'userData/registerUser',
+    async ({ username, email, password }, { dispatch }) => {
+        try {
+            const response = await axios.post(
+                'http://localhost:5000/api/users/register',
+                {
+                    username,
+                    userEmailAddress: email,
+                    userPassword: password,
+                }
+            );
+
+            // Dispatch action to set user data on successful registration
+            dispatch(
+                setUserData({
+                    username,
+                    userEmailAddress: email,
+                })
+            );
+            toast.success(response.data.message);
+            return response.data;
+        } catch (error) {
+            const errorMessage =
+                error.response?.data?.message || 'Unknown registration error';
+            toast.error(errorMessage);
+        }
+    }
+);
 
 export const userDataSlice = createSlice({
-    name: "userDataSlice",
+    name: 'userDataSlice',
     initialState: {
         username: null,
-        email: null,
+        userEmailAddress: null,
     },
     reducers: {
         setUserData: (state, action) => {
-            let { username, email } = action.payload;
-            console.log(action.payload);
-            [state.username, state.email] = [username, email];
-        }
-    }
+            let { username, userEmailAddress } = action.payload;
+            [state.username, state.userEmailAddress] = [
+                username,
+                userEmailAddress,
+            ];
+        },
+        logoutUser: (state) => {
+            state.username = null;
+            state.userEmailAddress = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(registerUser.fulfilled, (state, action) => {});
+        builder.addCase(registerUser.rejected, (state, action) => {});
+        builder.addCase(loginUser.fulfilled, (state, action) => {});
+        builder.addCase(loginUser.rejected, (state, action) => {});
+    },
 });
 
-export const { setUserData } = userDataSlice.actions;
+export const { setUserData, logoutUser } = userDataSlice.actions;
 export default userDataSlice.reducer;
