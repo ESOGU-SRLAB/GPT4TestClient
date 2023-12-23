32038d65165ebaed2a68d7819086fd313d84c2ea
Metin KONUK <metinkonuk@hotmail.com>
2023-12-23 19:04:08 +0300

    the first integration of monaco editor, compatible with react v18

--- src/components/unitTestGeneration/UnitTestGenerationPageComponent.jsx
+++ src/components/unitTestGeneration/UnitTestGenerationPageComponent.jsx
@@ -1,8 +1,13 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { fetchModelSettingsFromDB } from '../../redux/features/modelSettingsSlice';
 import FloatingButtonRight from './FloatingButtonRight';
 import RightSidebarComponent from './RightSidebarComponent';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import Editor from '@monaco-editor/react';
+import SplitPane, { Pane } from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
+import { Box, Paper } from '@mui/material';
+
 const UnitTestGenerationPageComponent = () => {
     const dispatch = useDispatch();
     const [isRightSidebarOpen, setRightSidebarOpen] = useState(false);
@@ -12,7 +17,15 @@ const UnitTestGenerationPageComponent = () => {
     useEffect(() => {
         dispatch(fetchModelSettingsFromDB());
     }, [dispatch]);
-
+    const [userCode, setUserCode] = useState('// type your code here');
+    const [generatedCode, setGeneratedCode] = useState(
+        '// AI generated code will appear here'
+    );
+    const [sizes, setSizes] = useState(['50%', 'auto']);
+    const handle = () => {
+        console.log(userCode);
+        console.log(generatedCode);
+    };
     return (
         <>
             {' '}
@@ -23,6 +36,37 @@ const UnitTestGenerationPageComponent = () => {
                 open={isRightSidebarOpen}
                 toggleSidebar={toggleRightSidebar}
             />
+            <Box
+                sx={{
+                    height: '70vh',
+                    mt: 2,
+                    mr: 2,
+                    ml: 2,
+                }}
+            >
+                <SplitPane split="vertical" sizes={sizes} onChange={setSizes}>
+                    <Pane minSize="40%" maxSize="60%">
+                        <Editor
+                            height="100%"
+                            theme="vs-light"
+                            language="python"
+                            value={userCode}
+                            onChange={setUserCode}
+                            loading={<span>LOADING THE EDITOR</span>}
+                        />
+                    </Pane>
+                    <Pane minSize="40%" maxSize="60%">
+                        <Editor
+                            height="100%"
+                            theme="vs-dark"
+                            language="python"
+                            value={generatedCode}
+                            options={{ readOnly: true }}
+                        />
+                    </Pane>
+                </SplitPane>
+            </Box>
+            <button onClick={handle}>CLICK ME</button>
         </>
     );
 };
